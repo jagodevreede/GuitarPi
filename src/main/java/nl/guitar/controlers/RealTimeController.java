2d33b4fd4e6507fe5887e08a3a67d05cc60a55b2
Jago de Vreede <jagodevreede@gmail.com>
2018-10-31 20:30:27 +0100

    Better nap time

--- src/main/java/nl/guitar/controlers/RealTimeController.java
+++ src/main/java/nl/guitar/controlers/RealTimeController.java
@@ -1,6 +1,6 @@
 package nl.guitar.controlers;
 
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.LockSupport;
 
 abstract class RealTimeController implements Controller {
     private long startTime;
@@ -10,29 +10,14 @@ abstract class RealTimeController implements Controller {
 
     public void waitUntilTimestamp(long timeStamp) {
         final long nextTimeStampInTime = startTime + timeStamp;
-        while (nextTimeStampInTime - System.currentTimeMillis() > 250) {
-            sleepMilliseconds(100);
-        }
-        while (nextTimeStampInTime - System.currentTimeMillis() > 0) {
-            // No op
-        }
+        LockSupport.parkUntil(nextTimeStampInTime);
     }
 
     public void waitMilliseconds(long waitTimeMS) {
-        final long nextTimeStampInTime = System.currentTimeMillis() + waitTimeMS;
-        while (nextTimeStampInTime - System.currentTimeMillis() > 250) {
-            sleepMilliseconds(100);
-        }
-        while (nextTimeStampInTime - System.currentTimeMillis() > 0) {
-            // No op
-        }
+        LockSupport.parkNanos(waitTimeMS * 1000000);
     }
 
-    private void sleepMilliseconds(long waitTimeMS) {
-        try {
-            TimeUnit.MILLISECONDS.sleep(waitTimeMS);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
+    public void sleepMilliseconds(long waitTimeMS) {
+        LockSupport.parkNanos(waitTimeMS * 1000000);
     }
 }
