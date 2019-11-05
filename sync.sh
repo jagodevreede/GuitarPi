723c8caf938213d82bbf79538ba254de9824c2e2
Jago de Vreede <jagodevreede@gmail.com>
2019-11-05 08:46:35 +0100

    No need to sync frontend project to pi

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 scp pi@guitarpi.local:/home/pi/guitar-player/*.conf ./
-rsync -avz --exclude 'target' --exclude '*.conf' ./ pi@guitarpi.local:/home/pi/guitar-player --delete
+rsync -avz --exclude 'target' --exclude '*.conf' --exclude 'frontend' ./ pi@guitarpi.local:/home/pi/guitar-player --delete
 
